80a3f6692b7d5c728f250fb7bec8e9b8a1abc681
Thibault Duplessis <t@lichess.org>
2022-11-18 13:37:51 +0100

    fix minimal rep

--- minimal.scala
+++ minimal.scala
@@ -3,14 +3,23 @@ import scala.concurrent.duration.*
 import reactivemongo.api.*
 
 object Minimal extends App:
+  run
+
+def run =
   given ExecutionContext = scala.concurrent.ExecutionContext.global
-
   val driver = new AsyncDriver
-  val connection =
-    Await.result(driver.connect(List("localhost:27017")), 2.seconds)
-  val db = Await.result(connection.database("test"), 2.seconds)
 
-  for i <- 0 to 600 do
+  (1 to 2).toList map { i =>
+    Await.result(
+      for
+        conn <- driver.connect(List("localhost:27017"))
+        db <- conn.database(s"test-rm-$i")
+      yield db,
+      3.seconds
+    )
+  }
+
+  for i <- 0 to 12 do
     println(i)
     Thread sleep 1000
 
